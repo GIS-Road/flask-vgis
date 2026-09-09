91af8d34f63cd346e55fbd8b92f892bb300ad233
Zhang-yizhang <1179402648@qq.com>
2026-09-09 13:50:47 +0800

    框架整合

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app-container">
+    <span>这是一个WebGIS展示系统</span>
+    <div>
+      <!-- 直接使用，无需导入 -->
+      <el-button type="primary">这是一个按钮</el-button>
+      <el-input v-model="inputValue" placeholder="请输入关键词" />
+    </div>
+  </div>
+</template>
+<script setup>
+import {onMounted,ref} from "vue";
+
+const inputValue = ref("")
+
+const props = defineProps({})
+onMounted(()=>{
+
+})
+</script>
+<style scoped>
+.app-container{
+  margin: 100px;
+  font-size: 24px;
+  font-weight: bold;
+  color: red;
+}
+</style>
---
+++ frontend/src/login.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="login-container">
+  登陆
+</div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+
+</style>
